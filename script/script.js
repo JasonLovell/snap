5d3a91e1f7f54daabaf820c825caa649c8ce4438
Jason Lovell <jasonlovelluk@outlook.com>
2019-11-20 15:44:42 +0000

    snap start

---
+++ script/script.js
@@ -0,0 +1,20 @@
+ var mixCards = parseInt(Math.round(Math.random()*9)+1);
+ var boxNumber = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9"]
+ var pack=[1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function dealCards() {
+
+    while (i < 10) {
+         document.getElementById(boxNumber[i]).innerHTML="Hello";  
+    i++;
+    }
+}
+
+
+
+
+
+
+
+
+
