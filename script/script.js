dbd0bcd69d5b8e43fa0c6168f32dabb8f0750597
Jason Lovell <jasonlovelluk@outlook.com>
2019-11-20 17:32:12 +0000

    update script

--- script/script.js
+++ script/script.js
@@ -1,13 +1,23 @@
- var mixCards = parseInt(Math.round(Math.random()*9)+1);
- var boxNumber = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9"]
- var pack=[1, 2, 3, 4, 5, 6, 7, 8, 9];
+var boxNumber;
+var pack;
+var placeCard;
+var i;
+var y;
+
+i=0;
+
+boxNumber = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9"];
+pack = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function dealCards() {
-
-    while (i < 10) {
-         document.getElementById(boxNumber[i]).innerHTML="Hello";  
-    i++;
+    document.getElementById("title").style.color = "red";
+    while(i < 9){
+        y = parseInt(Math.round(Math.random()*8)+1);
+        document.getElementById(boxNumber[i]).innerHTML=[y];
+        i++;
+        
     }
+    
 }
 
 
